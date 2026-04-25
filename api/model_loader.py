33f2db7939f05e2981f753a59e6813c7848d65fc
Debasish <debasishg2003@gmail.com>
2026-04-25 15:01:12 +0530

    modified

--- api/model_loader.py
+++ api/model_loader.py
@@ -1,38 +1,40 @@
 import os
 import time
+import logging
 import mlflow
+
 from src.config_loader import get_pipeline_config
 
+logger = logging.getLogger(__name__)
 
 config = get_pipeline_config("api")
 
 MODEL = None
 LAST_LOADED = 0
-REFRESH_INTERVAL = config["api"]["refresh_interval"]  # seconds
+REFRESH_INTERVAL = config["api"].get("refresh_interval", 60)  # default to 60 seconds if not set
 
 
 def get_model_uri():
-
-    #ENV variable can also be used to override the model URI if needed
+    # Env variable can override model URI if needed
     model_uri = os.getenv("MODEL_URI")
     if model_uri:
         return model_uri
 
-    # Fallback to params.yaml
-    config = get_pipeline_config("api")
     return config["mlflow"]["model_uri"]
 
-def get_model(model_path: str):
+
+def get_model():
     global MODEL, LAST_LOADED
 
     current_time = time.time()
+
     if MODEL is None or (current_time - LAST_LOADED) > REFRESH_INTERVAL:
         model_uri = get_model_uri()
-        print(f"Loading model from: {model_uri}")
+        logger.info(f"Loading model from: {model_uri}")
 
         MODEL = mlflow.pyfunc.load_model(model_uri)
         LAST_LOADED = current_time
     else:
-        print("Using cached model...")
+        logger.info("Using cached model")
 
     return MODEL
